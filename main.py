db83d955d542c3284b046d4a8a219dcff926b4a6
Soonhwan-Kwon <soouee@gmail.com>
2017-07-12 00:23:12 +0900

    fix reverted line

--- main.py
+++ main.py
@@ -82,9 +82,10 @@ def load_data(args):
         datagen = DataGenerator(save_dir=save_dir, model_name=model_name)
         datagen.load_train_data(data_json, max_duration=max_duration)
         datagen.load_validation_data(val_json, max_duration=max_duration)
-        if is_bi_graphemes and overwrite_bi_graphemes_dictionary:
-            load_labelutil(labelUtil=labelUtil, is_bi_graphemes=False, language=language)
-            generate_bi_graphemes_dictionary(datagen.train_texts+datagen.val_texts)
+        if is_bi_graphemes:
+            if not os.path.isfile("resources/unicodemap_en_baidu_bi_graphemes.csv") or overwrite_bi_graphemes_dictionary:
+                load_labelutil(labelUtil=labelUtil, is_bi_graphemes=False, language=language)
+                generate_bi_graphemes_dictionary(datagen.train_texts+datagen.val_texts)
         load_labelutil(labelUtil=labelUtil, is_bi_graphemes=is_bi_graphemes, language=language)
         args.config.set('arch', 'n_classes', str(labelUtil.get_count()))
 
